0a7fed0214c004339c2d232432792f8909d08d93
ProtikHore <protik.hore@gmail.com>
2021-10-03 12:39:52 +0600

    product delete

--- vuejs_frontend/src/components/product/ProductComponent.vue
+++ vuejs_frontend/src/components/product/ProductComponent.vue
@@ -62,12 +62,14 @@
                                             </router-link>
                                         </div>
                                         <div class="cursor-pointer w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                            </svg>
+                                            <router-link :to="{ name: 'product_edit', params: {id: product.id } }">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                </svg>
+                                            </router-link>
                                         </div>
                                         <div class="cursor-pointer w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <svg @click.prevent="deleteProduct(product.id, product.name, product.price)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
                                         </div>
@@ -78,7 +80,7 @@
                         <tbody v-else>
                             <tr>
                                 <td>
-                                    <h5 class="items-center mt-4 mb-4">No Visitors found.</h5>
+                                    <h5 class="items-center mt-4 mb-4">No Products found.</h5>
                                 </td>
                             </tr>
                         </tbody>
@@ -111,6 +113,19 @@ export default {
                 console.log(response);
                 this.products = response.data;
             });
+        },
+        deleteProduct(id, name, price) {
+            console.log(`button click delete this product: Name:  ${name} Price: ${price}`);
+            console.log(id);
+            if(confirm(`Are You sure want delete this product: Name:  ${name} Price: ${price}`)){
+                console.log('yes');
+                axios.post(`http://localhost:8000/api/product/delete/data/${id}`).then(response => {
+                    console.log(response);
+                    this.getProductData();
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
         }
     },
 
